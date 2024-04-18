3da3be400a1eacef30890c2232f500b0fce3311f
nunosantoss <nunongan.dev@gmail.com>
2024-04-18 01:19:24 +0100

    cors-anywhere #1

--- src/api/fetchRates.js
+++ src/api/fetchRates.js
@@ -1,11 +1,11 @@
-import SDK from './Initialization';
+import SDK from "./Initialization";
 
 export const fetchRates = async (currencyOne) => {
   try {
     const response = await SDK.getTicker(`${currencyOne}`);
     return response;
   } catch (error) {
-    console.error('Error fetching rates:', error);
+    console.error("Error fetching rates:", error);
     throw error;
   }
 };
